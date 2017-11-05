520b9aa47f97abdc47aba562473856a03736bbd1
Wakakusa Haruo <mail1@yoshihisa-b.sakura.ne.jp>
2017-11-05 16:01:14 +0900

    20171105 16:01

---
+++ 20130826_kuszi.lisp
@@ -0,0 +1,76 @@
+; SMPSUM - Iterated sums
+; http://www.spoj.com/problems/SMPSUM/
+
+; thanks to Doug Hoyte.
+
+(defun mkstr (&rest args)
+  (with-output-to-string (s)
+    (dolist (a args) (princ a s))))
+
+(defun symb (&rest args)
+  (values (intern (apply #'mkstr args))))
+
+(defun flatten (x)
+  (labels ((rec (x acc)
+             (cond ((null x) acc)
+                   ((atom x) (cons x acc))
+                   (t (rec
+                        (car x)
+                        (rec (cdr x) acc))))))
+    (rec x nil)))
+
+(defun g!-symbol-p (s)
+  (and (symbolp s)
+       (> (length (symbol-name s)) 2)
+       (string= (symbol-name s)
+                "G!"
+                :start1 0
+                :end1 2)))
+
+(defmacro defmacro/g! (name args &rest body)
+  (let ((syms (remove-duplicates
+                (remove-if-not #'g!-symbol-p
+                               (flatten body)))))
+    `(defmacro ,name ,args
+       (let ,(mapcar
+               (lambda (s)
+                 `(,s (gensym ,(subseq
+                                 (symbol-name s)
+                                 2))))
+               syms)
+         ,@body))))
+
+(defun o!-symbol-p (s)
+  (and (symbolp s)
+       (> (length (symbol-name s)) 2)
+       (string= (symbol-name s)
+                "O!"
+                :start1 0
+                :end1 2)))
+
+(defun o!-symbol-to-g!-symbol (s)
+  (symb "G!"
+        (subseq (symbol-name s) 2)))
+
+(defmacro defmacro! (name args &rest body)
+  (let* ((os (remove-if-not #'o!-symbol-p args))
+         (gs (mapcar #'o!-symbol-to-g!-symbol os)))
+    `(defmacro/g! ,name ,args
+       `(let ,(mapcar #'list (list ,@gs) (list ,@os))
+          ,(progn ,@body)))))
+
+;
+; main code
+;
+
+(defmacro! square (o!x)
+  `(* ,g!x ,g!x))
+
+(defmacro! sum-square (o!from o!to)
+  `(let ((,g!sum 0))
+    (loop for ,g!i from ,g!from to ,g!to do (setf ,g!sum (+ ,g!sum
+                                                            (* ,g!i ,g!i))))
+    ,g!sum))
+
+(print (sum-square (read) (read)))
+
