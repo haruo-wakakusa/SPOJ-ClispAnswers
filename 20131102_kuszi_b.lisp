d49a07cffb12e6e87a3f979a0988b94f3fd5597b
Wakakusa Haruo <mail1@yoshihisa-b.sakura.ne.jp>
2017-12-08 00:11:36 +0900

    20171208 0:11

--- 20131102_kuszi_b.lisp
+++ 20131102_kuszi_b.lisp
@@ -1,12 +1,16 @@
 ; SPTTRN3 - Straight Line Spiral Pattern (Act 3)
 ; http://www.spoj.com/problems/SPTTRN3/
 
-; this is not completed.
+; this is not completed
 
 (defstruct pt (x 0) (y 0))
 
 (defun pt+ (a b)
   (make-pt :x (+ (pt-x a) (pt-x b)) :y (+ (pt-y a) (pt-y b))))
+(defun pt- (a b)
+  (make-pt :x (- (pt-x a) (pt-x b)) :y (- (pt-y a) (pt-y b))))
+(defun pt= (a b)
+  (and (= (pt-x a) (pt-x b)) (= (pt-y a) (pt-y b))))
 (defmacro pt-ref (ary pt) `(aref ,ary (pt-y ,pt) (pt-x ,pt)))
 
 (defconstant +r1+ (make-pt :x 1 :y 0)) ; right
@@ -47,17 +51,32 @@
   (list (make-pt :x 1 :y 0) (make-pt :x 0 :y 1)
         (make-pt :x -1 :y 0) (make-pt :x 0 :y -1)))
 
+; contact check for endpoint
 (defun in-contact-p (ary pt char)
   (let* ((neighbor (mapcar (lambda (delta) (pt+ pt delta)) +4-neighbor+))
          (val (mapcar (lambda (pt) (pt-ref ary pt)) neighbor))
          (is-same (mapcar (lambda (val) (if (char= val char) 1 0)) val)))
-    (> (apply #'+ is-same) 2)))
+    (> (apply #'+ is-same) 1)))
 
-(defconstant +clockwise+
-  #( (make-pt :x -1 :y -1) (make-pt :x 0 :y -1)
-     (make-pt :x 1 :y -1) (make-pt :x 1 :y 0)
-     (make-pt :x 1 :y 1) (make-pt :x 0 :y 1)
-     (make-pt :x -1 :y 1) (make-pt :x -1 :y 0) ) )
+(defconstant +clockwise+ (make-array 8 :initial-contents (list
+  (make-pt :x -1 :y -1) (make-pt :x 0 :y -1)
+  (make-pt :x 1 :y -1) (make-pt :x 1 :y 0)
+  (make-pt :x 1 :y 1) (make-pt :x 0 :y 1)
+  (make-pt :x -1 :y 1) (make-pt :x -1 :y 0))))
+
+(defun connected-near-point-p (ary pt char)
+  (let ((cnt) (rise 0))
+    (setf cnt (loop for i below 8 sum
+      (if (char= (pt-ref ary (pt+ pt (aref +clockwise+ i))) char) 1 0)))
+    (when (zerop cnt) (return-from connected-near-point-p nil))
+    (when (= cnt 8) (return-from connected-near-point-p t))
+    (do ((cur 0 (mod (1+ cur) 8)) (prev 7 (mod (1+ prev) 8)) (i 0 (1+ i)))
+        ((= i 8))
+      (when
+        (and (not (char= (pt-ref ary (pt+ pt (aref +clockwise+ prev))) char))
+             (char= (pt-ref ary (pt+ pt (aref +clockwise+ cur))) char))
+        (incf rise)))
+    (= rise 1)))
 
 (defun make-test-case-array (size)
   (let ((ary (make-array (list (+ (pt-y size) 4) (+ (pt-x size) 4)))))
@@ -73,28 +92,24 @@
     ary))
 
 (defun walk-asap (ary init dir)
-  (do* ((p init (pt+ p dir))
-        (n1 (pt+ init dir) (pt+ n1 dir))
-        (n2 (pt+ n1 dir) (pt+ n2 dir))
-        (cnt 0 (1+ cnt)))
-       ((or (char= (pt-ref ary n1) #\*) (char= (pt-ref ary n2) #\*))
-        (setf (pt-ref ary p) #\*)
-        (if (>= cnt 2) p nil))
+  (do ((p (pt+ init dir) (pt+ p dir)))
+      ((or (in-contact-p ary p #\*)
+           (not (connected-near-point-p ary p #\.)))
+       (pt- p dir))
     (setf (pt-ref ary p) #\*)))
 
 (defun walk-a-little (ary init dir)
-  (let* ((n1 (pt+ init dir)) (n2 (pt+ n1 dir)) (n3 (pt+ n2 dir)))
-    (when (or (char= (pt-ref ary n1) #\*)
-              (char= (pt-ref ary n2) #\*))
-      (return-from walk-a-little nil))
-    (setf (pt-ref ary n1) #\*)
-    (when (char= (pt-ref ary n3) #\*) (return-from walk-a-little nil))
-    (setf (pt-ref ary n2) #\*)
-    n2))
+  (do ((p (pt+ init dir) (pt+ p dir)) (i 0 (1+ i)))
+      ((or (= i 2)
+           (in-contact-p ary p #\*)
+           (not (connected-near-point-p ary p #\.)))
+       (and (= i 2) (pt- p dir)))
+    (setf (pt-ref ary p) #\*)))
 
 (defun walkable-p (ary init dir)
-  (let* ((n1 (pt+ init dir)) (n2 (pt+ n1 dir)))
-    (not (or (char= (pt-ref ary n1) #\*) (char= (pt-ref ary n2) #\*)))))
+  (let ((next (pt+ init dir)))
+    (and (not (in-contact-p ary next #\*))
+         (connected-near-point-p ary next #\.))))
 
 (defun walk (ary init dir)
   (let ((p (copy-pt init)) (d dir))
@@ -115,11 +130,12 @@
 (defun solve-test-case (n)
   (let* ((size (make-pt :x n :y n))
          (ary (make-test-case-array size)))
+    (setf (pt-ref ary (make-pt :x 2 :y 2)) #\*)
     (walk ary (make-pt :x 2 :y 2) +r1+)
     (print-test-case ary size)))
 
-'(defvar *t* (read))
-'(dotimes (i *t*)
+(defvar *t* (read))
+(dotimes (i *t*)
   (solve-test-case (read))
   (unless (= i (1- *t*)) (write-char #\Newline)))
 
