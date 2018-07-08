084903620571aa08e77805012e45cfea99f46c83
Wakakusa Haruo <mail1@yoshihisa-b.sakura.ne.jp>
2018-07-08 17:52:51 +0900

    20180708 17:52

--- 20040508_adrian.lisp
+++ 20040508_adrian.lisp
@@ -1,6 +1,8 @@
 ; ARITH - Simple Arithmetics
 ; https://www.spoj.com/problems/ARITH/
 
+; this code has bug.
+
 (defun divide (str)
   (let ((pos (or (position #\+ str) (position #\- str) (position #\* str))))
     (list (substring str 0 pos)
@@ -14,8 +16,8 @@
     (make-string right :initial-element #\Space)))
 
 (defun print+ (arg1 arg2 op op-str)
-  (let* ((res (format nil "~a" (funcall op (read-from-string arg1)
-                                           (read-from-string arg2))))
+  (let* ((res (format nil "~a" (funcall op (parse-integer arg1)
+                                           (parse-integer arg2))))
          (+arg2 (concatenate 'string op-str arg2))
          (cols (max (length arg1) (length +arg2) (length res))))
   (format* arg1 cols 0)
@@ -23,12 +25,55 @@
   (format t "~a~%" (make-string cols :initial-element #\-))
   (format* res cols 0)))
 
-(defun get-mult-series (arg1 arg2 n1 n2)
-  (map 'list (lambda (c)
+(defmacro -> (init &rest forms)
+  (let ((forms! (loop for form in forms collect `(setq $ ,form))))
+    `(let (($ ,init))
+      ,@forms!
+      $)))
 
-;(defun print* (arg1 arg2)
-;  (let* ((n1 (parse-integer arg1))
-;         (n2 (parse-integer arg2))
-;         (res (format nil "~a" (* n1 n2)))
-;         (+arg2 (format nil "*~a" arg2))
-;         (
+(defun get-mult-series (arg1 arg2 n1 n2)
+  (-> arg2
+      (reverse $)
+      (map 'list #'string $)
+      (mapcar #'parse-integer $)
+      (mapcar (lambda (n) (* n1 n)) $)
+      (mapcar (lambda (n) (format nil "~a" n)) $)))
+
+(defun shift-left (str-list)
+  (do* ((lst str-list (rest lst)) (i 0 (1+ i)) (res nil))
+       ((null lst) (reverse res))
+    (setq res (cons (format nil "~a~a"
+                            (first lst)
+                            (make-string i :initial-element #\Space))
+                    res))))
+
+(defun print* (arg1 arg2)
+  (let* ((n1 (parse-integer arg1))
+         (n2 (parse-integer arg2))
+         (*arg2 (format nil "*~a" arg2))
+         (temporary-exps (shift-left (get-mult-series arg1 arg2 n1 n2)))
+         (res (format nil "~a" (* n1 n2)))
+         (tmp-max (-> temporary-exps
+                      (mapcar #'length $)
+                      (apply #'max $)))
+         (max (max (length arg1) (length *arg2) tmp-max (length res))))
+    (format* arg1 max 0)
+    (format* *arg2 max 0)
+    (format* (make-string (max (length arg1) (length *arg2))
+                          :initial-element #\-)
+             max 0)
+    (dolist (exp temporary-exps) (format* exp max 0))
+    (unless (= (length temporary-exps) 1)
+      (format t "~a~%" (make-string max :initial-element #\-))
+      (format* res max 0))))
+
+(defvar *t* (parse-integer (read-line)))
+(dotimes (i *t*)
+  (let ((exp (divide (read-line))))
+    (cond
+      ((string= (second exp) "+") (print+ (first exp) (third exp) #'+ "+"))
+      ((string= (second exp) "-") (print+ (first exp) (third exp) #'- "-"))
+      ((string= (second exp) "*") (print* (first exp) (third exp)))
+      (t (error ""))))
+  (write-char #\Newline))
+
