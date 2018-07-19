92344dddd9b970365d06e31c0dc89c7887b55d94
Wakakusa Haruo <mail1@yoshihisa-b.sakura.ne.jp>
2018-07-19 21:23:55 +0900

    20180719 21:23

--- 20040508_adrian_b.lisp
+++ 20040508_adrian_b.lisp
@@ -1,156 +1,97 @@
 ; BULK - The Bulk!
 ; https://www.spoj.com/problems/BULK/
 
-(defmacro block* (&rest forms)
-  (let ((vars nil) (funs nil))
-    (dolist (form forms)
-      (cond
-        ((null form))
-        ((atom form))
-        ((eq (first form) 'var)
-         (do ((rest (rest form) (cddr rest)))
-             ((null rest))
-           (setq vars (cons (first rest) vars))))
-        ((eq (first form) 'fun)
-         (setq funs (cons (rest form) funs)))))
-    (let ((forms (remove-if (lambda (form) (and (not (atom form))
-                                                (eq (first form) 'fun)))
-                            forms)))
-      (let ((forms (mapcar (lambda (form) (cond ((atom form) form)
-                                                ((eq (first form) 'var)
-                                                 `(setq ,@(rest form)))
-                                                (t form)))
-                           forms)))
-        `((lambda ()
-           (let (,@(mapcar #'list (reverse vars)))
-             (labels (,@funs)
-               ,@forms))))))))
+;                      [Problem]
+;             *-----*  @ A bulk is an assembly of cubes.
+;            /     /|  @ The input is informations of the bulk surface(face).
+;           /     / |  @ The output is its own volume.
+;      *---/     /  *
+;     /   *-----*  /   [Note]
+;    /    |     | *    @ Bulks can have some empty spaces.
+;   *-----*     | |      (ex. a box in a box)
+;   |           | *    @ Bulks can be non-path-connected.
+;   |           |/       (ex. a "C"-shaped instance)
+;   *-----------*
+;                      [Strategy]
+;         *-----*      @ In view from any orientation, any faces has own
+;         |     |        pair. Or, the number of faces are absolutely even,
+;   *-----*-----*        not odd number for each unit.
+;   |     *-----*      @ We use a grid instead of the exact projection
+;   |     |     |        in order to simplify the algorithm.
+;   *-----*-----*      @ We treat only faces pallalel to YZ-plane
+;                        (We are viewing it from x = -INF).
+;   *-----*-----*      @ For each grid segment, we calculate the depth of
+;   |     |     |        the bulk due to counting the crossing faces.
+;   *-----*-----*
+;   *-----*-----*      @ The above 3D-problem reduces a 2D-problem. 
+;   |     |     |        "Which is a grid segment in a face, or not?"
+;   *-----*-----*      @ We solve it to count the crossing edges pallalel to
+;                        Z-axis.
+
+(defmacro pipe (init &rest forms)
+"Like F#'s pipeline-operator or Clojure's threading macro. The forms
+evaluate in order. The variable $ are the init value or the value of former
+expression."
+  (do* ((rest forms (cdr rest)) (res init))
+       ((null rest) res)
+    (setq res `(let (($ ,res)) ,(car rest)))))
 
 
-(defmacro -> (init &rest forms)
-  (let ((forms! (loop for form in forms collect `(setq $ ,form))))
-    `(let (($ ,init))
-      ,@forms!
-      $)))
+(defstruct point (x) (y) (z))
+(defstruct z-range (start) (end))
 
+(defun list-shift-right (list)
+"Example: '(1 2 3 4) -> '(4 1 2 3)"
+  (cons (first (last list)) (butlast list)))
 
-(defstruct y-segment (start 0 :type fixnum) (end 0 :type fixnum))
-(defstruct z-segment (start 0 :type fixnum) (end 0 :type fixnum))
-(defstruct segment//z-axis (x 0 :type fixnum) (y 0 :type fixnum)
-                           (z-start 0 :type fixnum)
-                           (z-end 0 :type fixnum))
+(defun list-shift-left (list)
+"Example: '(1 2 3 4) -> '(2 3 4 1)"
+  (append (rest list) (list (first list))))
+
+(defun get-orientation (p1 p2)
+"For the path from the point p1 to the point p2, returns the orientation
+(:x, :y or :z)"
+  (let ((dx (- (point-x p2) (point-x p1)))
+        (dy (- (point-y p2) (point-y p1)))
+        (dz (- (point-z p2) (point-z p1))))
+    (cond ((and (not (zerop dx)) (zerop dy) (zerop dz)) :x)
+          ((and (zerop dx) (not (zerop dy)) (zerop dz)) :y)
+          ((and (zerop dx) (zerop dy) (not (zerop dz))) :z)
+          (t (error "error in get-orientation")))))
+
+(defun remove-if-not-edge (face)
+  (loop for cur in face
+        and left in (list-shift-left face)
+        and right in (list-shift-right face)
+        unless (eq (get-orientation cur left) (get-orientation cur right))
+        collect cur))
+
+(defun pallalel-to-yz-plane-p (face)
+  (notany (lambda (p1 p2) (eq (get-orientation p1 p2) :x))
+          face
+          (list-shift-right face)))
+
+(defun remove-if-not-pallalel-to-yz-plane (faces)
+  (remove-if-not #'pallalel-to-yz-plane-p faces))
+
+(defun get-grid-points (faces axis-accessor)
+  (pipe faces
+        (apply #'append $)
+        (mapcar axis-accessor $)
+        (remove-duplicates $)
+        (sort $ #'<)))
 
 (defun read-testcase ()
-  (loop for nil below (read) collect
-    (loop for nil below (read) collect (list (read) (read) (read)))))
+  (loop for i below (read) collect
+    (loop for j below (read) collect
+      (make-point :x (read) :y (read) :z (read)))))
 
-
-(defun get-segment (plane)
-  (remove-if #'null
-    (mapcar
-      (lambda (p1 p2)
-        (if (or (not (= (first p1) (first p2)))
-                (not (= (second p1) (second p2))))
-            nil
-            (make-segment//z-axis :x (first p1)
-                                  :y (second p1)
-                                  :z-start (min (third p1) (third p2))
-                                  :z-end (max (third p1) (third p2)))))
-      plane
-      (cons (first (last plane)) plane))))
-
-(defun yz-segment-in-plane-p (y-segment z-segment plane)
-  (let ((count 0))
-    (dolist (segment (get-segment plane))
-      (when (and (<= (segment//z-axis-y segment) (y-segment-start y-segment))
-                 (<= (segment//z-axis-z-start segment)
-                     (z-segment-start z-segment))
-                 (> (segment//z-axis-z-end segment)
-                    (z-segment-start z-segment)))
-        (incf count)))
-    (oddp count)))
-
-(defun get-planes-including-yz-segment-in (planes//yz-plane
-                                           y-segment
-                                           z-segment)
-  (remove-if-not (lambda (plane) (yz-segment-in-plane-p y-segment
-                                                        z-segment
-                                                        plane))
-                 planes//yz-plane))
-
-(defun get-x-depth-for-yz-segment (planes//yz-plane y-segment z-segment)
-  (let ((planes (get-planes-including-yz-segment-in planes//yz-plane
-                                                    y-segment
-                                                    z-segment)))
-    (unless (evenp (length planes))
-      (error "error in get-x-depth-for-yz-segment"))
-    (setq planes (sort planes (lambda (p1 p2) (< (caar p1) (caar p2)))))
-    (do* ((rest planes (cddr rest)) (res 0))
-         ((null rest) res)
-      (incf res (- (caar (second rest)) (caar (first rest)))))))
-
-
-(defun solve-testcase (planes)
-  (block*
-
-    (format t "~%MAKING POINTS...~%")
-    (var points
-      (time (apply #'append planes)))
-    ;(format t "points = ~a~%" points)
-
-    (fun is-x-constant (plane)
-      (every (lambda (p1 p2) (= (first p1) (first p2))) plane (rest plane)))
-
-    (format t "~%MAKING PLANES//YZ-PLANE...~%")
-    (var planes//yz-plane
-      (time (remove-if-not #'is-x-constant planes)))
-    ;(format t "planes//yz-plane = ~a~%" planes//yz-plane)
-
-    (format t "~%MAKING Y-GRID...~%")
-    (var y-grid
-      (time
-      (-> points
-          (mapcar #'second $)
-          (remove-duplicates $)
-          (sort $ #'<)
-          (mapcar (lambda (start end) (make-y-segment :start start :end end))
-                  $
-                  (rest $)))))
-    ;(format t "y-grid = ~a~%" y-grid)
-
-    (format t "~%MAKING Z-GRID...~%")
-    (var z-grid
-      (time
-      (-> points
-          (mapcar #'third $)
-          (remove-duplicates $)
-          (sort $ #'<)
-          (mapcar (lambda (start end) (make-z-segment :start start :end end))
-                  $
-                  (rest $)))))
-    ;(format t "z-grid = ~a~%" z-grid)
-
-    (format t "~%CALCULATING VOLUME...~%")
-    (var volume
-      (time
-      (loop for y-segment in y-grid sum
-        (* (- (y-segment-end y-segment) (y-segment-start y-segment))
-           (loop for z-segment in z-grid sum
-             (* (- (z-segment-end z-segment)
-                   (z-segment-start z-segment))
-                (get-x-depth-for-yz-segment planes//yz-plane
-                                            y-segment
-                                            z-segment)))))))
-
-    volume
-
-
-  ) ; end of block*
-) ; end of defun<solve-testcase>
-
-
-(defvar *t* (read))
-(dotimes (i *t*)
-  (format t "The bulk is composed of ~a units.~%"
-    (solve-testcase (read-testcase))))
+(defun solve-testcase ()
+  (let* ((raw-faces (read-testcase))
+         (faces (pipe raw-faces
+                      (remove-if-not-pallalel-to-yz-plane $)
+                      (mapcar #'remove-if-not-edge $)))
+         (y-grid-points (get-y-grid-points faces #'point-y))
+         (z-grid-points (get-z-grid-points faces #'point-z)))
+    z-grid-points))
 
