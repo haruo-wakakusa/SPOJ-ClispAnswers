2ffec0c9e0fdd9027f79c80d85459e6152b0ed84
Wakakusa Haruo <mail1@yoshihisa-b.sakura.ne.jp>
2018-07-23 22:40:08 +0900

    20180723 22:40

--- 20040508_adrian_c.lisp
+++ 20040508_adrian_c.lisp
@@ -12,44 +12,28 @@
       :c (read)
       :x-list (loop for nil below s collect (read)))))
 
-(defun make-gauss-matrix (testcase)
-  (let ((mat (make-array (list (testcase-s testcase)
-                               (1+ (testcase-s testcase))))))
-    (loop for i below (testcase-s testcase) do
-      (loop for j below (testcase-s testcase) do
-        (setf (aref mat i j) (expt (1+ i) j)))
-      (setf (aref mat i (testcase-s testcase))
-            (nth i (testcase-x-list testcase))))
-    mat))
+(defun make-difference-table (testcase)
+  (let ((table (make-array (testcase-s testcase))))
+    (setf (aref table 0) (reverse (testcase-x-list testcase)))
+    (loop for i from 1 below (testcase-s testcase) do
+      (setf (aref table i) (mapcar #'- (aref table (1- i))
+                                       (rest (aref table (1- i))))))
+  table))
 
-(defun matrix-row-mult (mat testcase row magnitude)
-  (loop for j to (testcase-s testcase) do
-    (setf (aref mat row j) (* magnitude (aref mat row j)))))
+(defun expand-difference-table (testcase table)
+  (setf (aref table (1- (testcase-s testcase)))
+        (cons (first (aref table (1- (testcase-s testcase))))
+              (aref table (1- (testcase-s testcase)))))
+  (loop for i from (- (testcase-s testcase) 2) downto 0 do
+    (setf (aref table i)
+          (cons (+ (first (aref table i)) (first (aref table (1+ i))))
+                (aref table i)))))
 
-(defun matrix-row-add (mat testcase dst-row src-row delta)
-  (loop for j to (testcase-s testcase) do
-    (incf (aref mat dst-row j) (* (aref mat src-row j) delta))))
-
-(defun row-reduction (mat testcase row)
-  (matrix-row-mult mat testcase row (/ (aref mat row row)))
-  (loop for i below (testcase-s testcase) unless (= i row) do
-    (matrix-row-add mat testcase i row (- (aref mat i row)))))
-
-(defun get-polynomial-value (mat testcase n)
-  (loop for j below (testcase-s testcase) sum
-    (* (aref mat j (testcase-s testcase)) (expt n j))))
-
-(defun solve-testcase ()
+(dotimes (i (read))
   (let* ((testcase (read-testcase))
-         (matrix (make-gauss-matrix testcase)))
-    (loop for i below (testcase-s testcase) do
-      (row-reduction matrix testcase i))
+         (table (make-difference-table testcase)))
+    (dotimes (j (testcase-c testcase))
+      (expand-difference-table testcase table))
     (format t "~{~a~^ ~}~%"
-      (mapcar (lambda (n) (get-polynomial-value matrix testcase n))
-              (loop for i
-                    from (1+ (testcase-s testcase))
-                    to (+ (testcase-s testcase) (testcase-c testcase))
-                    collect i)))))
-
-(dotimes (i (read)) (solve-testcase))
+      (reverse (subseq (aref table 0) 0 (testcase-c testcase))))))
 
